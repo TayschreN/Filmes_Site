66ee10c920b3e9b9618cdd24db2da9ff589d8f45
Gabriel <gabrielfsilva2609@gmail.com>
2023-08-03 12:10:02 -0300

    os bagulhos

---
+++ fimins.sql
@@ -0,0 +1,32 @@
+﻿CREATE TABLE tableName 
+(
+    codigoFilme	INT,
+    	VARCHAR(512),
+    nome_Filme	VARCHAR(512),
+    	VARCHAR(512),
+    	VARCHAR(512),
+    sinopse	VARCHAR(512),
+    	VARCHAR(512),
+    produtora	VARCHAR(512),
+    	VARCHAR(512),
+    data_Lancamento	INT,
+    	VARCHAR(512),
+    	VARCHAR(512),
+    genero	VARCHAR(512),
+    	VARCHAR(512),
+    idioma	VARCHAR(512),
+    	VARCHAR(512),
+    receita	INT
+);
+
+INSERT INTO tableName (codigoFilme, , nome_Filme, , , sinopse, , produtora, , data_Lancamento, , , genero, , idioma, , receita) VALUES ('1', '', 'El Camino', '', '', 'Maneiro', '', 'qualquer uma', '', '8012005', '', '', 'Ficção', '', 'Ingles', '', '1000');
+INSERT INTO tableName (codigoFilme, , nome_Filme, , , sinopse, , produtora, , data_Lancamento, , , genero, , idioma, , receita) VALUES ('2', '', 'Carros', '', '', 'Daorinha', '', 'arroz', '', '20202010', '', '', 'Animação', '', 'Ingles', '', '20000');
+INSERT INTO tableName (codigoFilme, , nome_Filme, , , sinopse, , produtora, , data_Lancamento, , , genero, , idioma, , receita) VALUES ('3', '', 'O Exorcista', '                       ', '', 'Terrormassa', '', 'feijao records', '', '34212002', '', '', 'Terror', '', 'Ingles', '', '3000');
+INSERT INTO tableName (codigoFilme, , nome_Filme, , , sinopse, , produtora, , data_Lancamento, , , genero, , idioma, , receita) VALUES ('4', '', 'Tomates', '', '', 'Esqusiito', '', 'paiosao', '', '9112001', '', '', 'Trash', '', 'Ingles', '', '4000');
+INSERT INTO tableName (codigoFilme, , nome_Filme, , , sinopse, , produtora, , data_Lancamento, , , genero, , idioma, , receita) VALUES ('5', '', 'V8', '', '', 'seloco', '', 'mopa', '', '20102002', '', '', 'Ficção', '', 'Ingles', '', '6000');
+INSERT INTO tableName (codigoFilme, , nome_Filme, , , sinopse, , produtora, , data_Lancamento, , , genero, , idioma, , receita) VALUES ('6', '', 'Psicose', '', '', 'trve', '', 'trve', '', '11112011', '', '', 'Terror', '', 'Ingles', '', '4000');
+INSERT INTO tableName (codigoFilme, , nome_Filme, , , sinopse, , produtora, , data_Lancamento, , , genero, , idioma, , receita) VALUES ('7', '', 'Rogue One', '', '', 'paia', '', 'sigma', '', '12122012', '', '', 'Ficção', '', 'Ingles', '', '54544');
+INSERT INTO tableName (codigoFilme, , nome_Filme, , , sinopse, , produtora, , data_Lancamento, , , genero, , idioma, , receita) VALUES ('8', '', 'From Dusk till Dawn', '', '', 'maneiirnho', '', 'nofap', '', '26122005', '', '', 'Drama', '', 'Ingles', '', '121212');
+INSERT INTO tableName (codigoFilme, , nome_Filme, , , sinopse, , produtora, , data_Lancamento, , , genero, , idioma, , receita) VALUES ('9', '', 'Sobre Meninos e Lobos', '', '', 'slk', '', 'pajas', '', '98082006', '', '', 'Suspense', '', 'Ingles', '', '2323');
+INSERT INTO tableName (codigoFilme, , nome_Filme, , , sinopse, , produtora, , data_Lancamento, , , genero, , idioma, , receita) VALUES ('10', '', 'Sharknado', '', '', 'chad', '', 'comores', '', '20201999', '', '', 'Trash', '', 'Ingles', '', '6456');
+INSERT INTO tableName (codigoFilme, , nome_Filme, , , sinopse, , produtora, , data_Lancamento, , , genero, , idioma, , receita) VALUES ('11', '', 'A Casa de Cera', '', '', 'sigmazinho', '', 'kuwait', '', '28121994', '', '', 'Terror', '', 'Ingles', '', '3252352');
